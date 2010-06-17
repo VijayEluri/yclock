77267619c1ef49656f3e9a948905dd407de70202
Yoshiharu Hirose <yh1224@gmail.com>
2010-06-18 00:27:03 +0900

    バージョン情報のテキストをファイルから取得するようにした

--- src/net/assemble/yclock/YclockAboutActivity.java
+++ src/net/assemble/yclock/YclockAboutActivity.java
@@ -1,14 +1,22 @@
 package net.assemble.yclock;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 import net.assemble.yclock.R;
 
@@ -32,6 +40,27 @@ public class YclockAboutActivity extends Activity
         getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
                 R.drawable.icon);
 
+        // テキスト
+        AssetManager as = getResources().getAssets();
+        try {
+            InputStream fin;
+            try {
+                fin = as.open("about_" + Locale.getDefault().getLanguage() + ".txt");
+            } catch (IOException e) {
+                fin = as.open("about.txt");
+            }
+            BufferedReader in = new BufferedReader(new InputStreamReader(fin));
+            StringBuffer str = new StringBuffer();
+            String s;
+            while ((s = in.readLine()) != null) {
+                str.append(s + "\n");
+            }
+            in.close();
+
+            TextView text = (TextView) findViewById(R.id.text);
+            text.setText(str.toString());
+        } catch (IOException e) {}
+
         // OK
         Button btn_ok = (Button) findViewById(R.id.ok);
         btn_ok.setOnClickListener(new View.OnClickListener() {
@@ -41,7 +70,7 @@ public class YclockAboutActivity extends Activity
             }
             
         });
-        
+
         // Send report 
         Button btn_report = (Button) findViewById(R.id.report);
         btn_report.setOnClickListener(new View.OnClickListener() {
