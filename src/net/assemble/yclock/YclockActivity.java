d40ea9d760d271f018cd80e99d6a589894bf1122
Yoshiharu Hirose <yh1224@gmail.com>
2010-07-11 01:35:35 +0900

    不要コード削除

--- src/net/assemble/yclock/YclockActivity.java
+++ src/net/assemble/yclock/YclockActivity.java
@@ -12,7 +12,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ToggleButton;
 
 import net.assemble.yclock.R;
