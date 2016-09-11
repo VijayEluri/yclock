11b0b0802e9a941442a735342a5ce083d682f55e
Yoshiharu Hirose <yh1224@gmail.com>
2016-09-11 17:55:36 +0900

    Handle alarm by IntentService

--- app/src/main/java/net/assemble/yclock/YclockAlarmService.java
+++ app/src/main/java/net/assemble/yclock/YclockAlarmService.java
@@ -1,33 +1,44 @@
 package net.assemble.yclock;
 
-import java.util.Calendar;
+import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
-import android.content.BroadcastReceiver;
 import android.os.PowerManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import java.util.Calendar;
+
 /**
  * Receiver for Alarm Intent
  */
-public class YclockAlarmReceiver extends BroadcastReceiver
+public class YclockAlarmService extends IntentService
 {
+    private static final String TAG = YclockAlarmService.class.getSimpleName();
+
+    public YclockAlarmService(String name) {
+        super(name);
+    }
+
+    @SuppressWarnings("unused")
+    public YclockAlarmService() {
+        this(TAG);
+    }
+
     @Override
-    public void onReceive(Context ctx, Intent intent) {
+    public void onHandleIntent(Intent intent) {
         Log.d(Yclock.TAG, "received intent: " + intent.getAction());
 
         if (Calendar.getInstance().get(Calendar.MINUTE) % 30 == 0) {
-            TelephonyManager tel = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+            TelephonyManager tel = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
             if (tel.getCallState() == TelephonyManager.CALL_STATE_OFFHOOK) {
                 // 通話中は抑止
                 return;
             }
-            PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
             PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, Yclock.TAG);
             wl.acquire(3000);
-            new YclockVoice(ctx).play();
+            new YclockVoice(this).play();
         }
     }
-
 }
